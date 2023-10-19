3c332f4c62cbf1fe714b8fe9b966d681c6b445d1
Hyun <han911116@gmail.com>
2023-10-19 16:56:42 +0900

    feat: 중복 테그 컴포넌트로 변경

---
+++ source/frontend/src/containers/common/shape.tsx
@@ -0,0 +1,12 @@
+const ShapeComponent = () => {
+    return (
+        <svg className="position-absolute start-0 bottom-0 flip-y" style={{color: 'var(--bs-dark)'}} width="100%" height="36"
+             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 152" fill="none" preserveAspectRatio="none">
+            <path
+                d="M126.597 138.74C99.8867 127.36 76.6479 109.164 59.2161 85.9798L0 3.05176e-05L1440 0C1440 0 1419.98 25.8404 1380.15 32.9584L211.382 150.811C182.549 154.283 153.308 150.12 126.597 138.74Z"
+                fill="currentColor" />
+        </svg>
+    )
+}
+
+export default ShapeComponent;
--- source/frontend/src/containers/home/home.tsx
+++ source/frontend/src/containers/home/home.tsx
@@ -7,6 +7,7 @@ import {RootState} from "@reducer/root.reducer.tsx";
 import {useEffect} from "react";
 import {onGetEventAction} from "@action/home.action.tsx";
 import {testData} from "@container/home/home.test.tsx";
+import ShapeComponent from "@container/common/shape.tsx";
 
 
 const HomeContainer = () => {
@@ -35,11 +36,7 @@ const HomeContainer = () => {
                         </div>
                     </div>
                 </div>
-                <svg className="position-absolute start-0 bottom-0 flip-y" style={{color: 'var(--bs-dark)'}}
-                     width="100%" height="36"
-                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 152" fill="none" preserveAspectRatio="none">
-                    <path d="M126.597 138.74C99.8867 127.36 76.6479 109.164 59.2161 85.9798L0 3.05176e-05L1440 0C1440 0 1419.98 25.8404 1380.15 32.9584L211.382 150.811C182.549 154.283 153.308 150.12 126.597 138.74Z" fill="currentColor"/>
-                </svg>
+                <ShapeComponent />
             </section>
         </main>
     )
